cf51786f2a3a040131aa31acdb0c0bffaa18d851
ahmedkmadani <ahmedkamal543@gmail.com>
2019-05-14 17:26:04 +0000

    login fun working

--- app/src/main/java/com/example/ampersandcontact/SiginActivity.kt
+++ app/src/main/java/com/example/ampersandcontact/SiginActivity.kt
@@ -4,7 +4,15 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.RequestFuture
+import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_signin.*
+import org.json.JSONObject
+
+val url = "https://ampersand-contact-exchange-api.herokuapp.com/api/v1/login"
 
 class SiginActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,21 +20,32 @@ class SiginActivity: AppCompatActivity() {
         setContentView(R.layout.activity_signin)
 
         btn_signin.setOnClickListener {
-
             Sigin()
         }
-
     }
 
     private fun Sigin() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+        val JsonObj = JSONObject()
 
         val user_email = input_email.text
         val user_pass = input_password.text
 
-        Toast.makeText(this@SiginActivity, user_email , Toast.LENGTH_LONG).show()
+        JsonObj.put("email", user_email)
+        JsonObj.put("password", user_pass)
 
-        val intentHome = Intent(this, HomeActivity::class.java)
-        startActivity(intentHome)
+        val que = Volley.newRequestQueue(this@SiginActivity)
+        val req = JsonObjectRequest(Request.Method.POST, url, JsonObj, Response.Listener {
+
+            Toast.makeText(this@SiginActivity, "login Successfully", Toast.LENGTH_LONG).show()
+
+        }, Response.ErrorListener {
+
+            Toast.makeText(this@SiginActivity, "login Failed", Toast.LENGTH_LONG).show()
+
+        })
+
+        que.add(req)
     }
+
 }
