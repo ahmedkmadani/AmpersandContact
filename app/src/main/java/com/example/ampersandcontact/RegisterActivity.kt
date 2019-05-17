49795978583f0c6919d39c798162a22f205cfd83
ahmedkmadani <ahmedkamal543@gmail.com>
2019-05-17 20:09:14 +0000

    working in login fun

--- app/src/main/java/com/example/ampersandcontact/RegisterActivity.kt
+++ app/src/main/java/com/example/ampersandcontact/RegisterActivity.kt
@@ -2,10 +2,56 @@ package com.example.ampersandcontact
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import kotlinx.android.synthetic.main.activity_register.*
+import org.json.JSONObject
+
+val url_register = "https://ampersand-contact-exchange-api.herokuapp.com/api/v1/register"
 
 class RegisterActivity: AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        btn_register.setOnClickListener {
+            register()
+        }
+    }
+
+    private fun register() {
+
+        val JsonObj = JSONObject()
+
+        val input_name = input_name.text
+        val input_email = input_email.text
+        val input_password = input_password.text
+        val input_phone = input_phone.text
+        val input_role = input_role.text
+        val input_twitter = input_twitter.text
+        val input_linkidn = input_linkedln.text
+
+        JsonObj.put("email", input_email)
+        JsonObj.put("password", input_password)
+        JsonObj.put("firstName", input_name)
+        JsonObj.put("lastName", input_name)
+        JsonObj.put("twitter", input_twitter)
+        JsonObj.put("website", input_role)
+
+        val que = Volley.newRequestQueue(this@RegisterActivity)
+        val req = JsonObjectRequest(Request.Method.POST, url_register, JsonObj, Response.Listener {
+
+            Toast.makeText(this@RegisterActivity, "Register Successfully", Toast.LENGTH_LONG).show()
+
+        }, Response.ErrorListener {
+
+            Toast.makeText(this@RegisterActivity, "Register Failed" , Toast.LENGTH_LONG).show()
+
+        })
+
+        que.add(req)
     }
 }
--- app/src/main/java/com/example/ampersandcontact/SiginActivity.kt
+++ app/src/main/java/com/example/ampersandcontact/SiginActivity.kt
@@ -1,8 +1,10 @@
 package com.example.ampersandcontact
 
+import android.app.ProgressDialog
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.widget.Toast
 import com.android.volley.Request
 import com.android.volley.Response
@@ -12,7 +14,7 @@ import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_signin.*
 import org.json.JSONObject
 
-val url = "https://ampersand-contact-exchange-api.herokuapp.com/api/v1/login"
+val url_sigin = "https://ampersand-contact-exchange-api.herokuapp.com/api/v1/login"
 
 class SiginActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,26 +28,77 @@ class SiginActivity: AppCompatActivity() {
 
     private fun Sigin() {
 
+        if (!validate()) {
+            return
+        }
+
+        val progressDialog = ProgressDialog(this@SiginActivity,com.example.ampersandcontact.R.style.AppTheme)
+        progressDialog.isIndeterminate = true
+        progressDialog.setMessage("Login...")
+        progressDialog.show()
+
+        val user_email = input_email!!.text.toString()
+        val user_pass = input_password!!.text.toString()
+
         val JsonObj = JSONObject()
-
-        val user_email = input_email.text
-        val user_pass = input_password.text
-
         JsonObj.put("email", user_email)
         JsonObj.put("password", user_pass)
 
         val que = Volley.newRequestQueue(this@SiginActivity)
-        val req = JsonObjectRequest(Request.Method.POST, url, JsonObj, Response.Listener {
+        val req = JsonObjectRequest(Request.Method.POST, url_sigin, JsonObj, Response.Listener <JSONObject> {response ->
 
-            Toast.makeText(this@SiginActivity, "login Successfully", Toast.LENGTH_LONG).show()
+
+            // code insert into database and create login session here
+
+            Log.d("Response", response.toString())
+
+            onSiginSuccess()
+            progressDialog.dismiss()
+
 
         }, Response.ErrorListener {
 
-            Toast.makeText(this@SiginActivity, "login Failed", Toast.LENGTH_LONG).show()
+            progressDialog.dismiss()
+            onSiginFailed()
 
         })
 
         que.add(req)
+    }
+
+
+    fun validate(): Boolean {
+        var valid = true
+
+        val email = input_email!!.text.toString()
+        val password = input_password!!.text.toString()
+
+        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            input_email!!.error = "enter a valid email address"
+            valid = false
+        } else {
+            input_email!!.error = null
+        }
+
+        if (password.isEmpty() || password.length < 2 || password.length > 10) {
+            input_password!!.error = "between 4 and 10 alphanumeric characters"
+            valid = false
+        } else {
+            input_password!!.error = null
+        }
+
+        return valid
+    }
+
+    fun onSiginSuccess(){
+
+        Toast.makeText(this@SiginActivity, "login Successfully", Toast.LENGTH_LONG).show()
+        startActivity(Intent(this, HomeActivity::class.java))
+    }
+
+    fun onSiginFailed(){
+
+        Toast.makeText(this@SiginActivity, "login Failed", Toast.LENGTH_LONG).show()
     }
 
 }
